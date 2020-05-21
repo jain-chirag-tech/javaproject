b1d81069d1e21f376029260e18ac8e393a656110
chirag <chiragjn807@gmail.com>
2020-05-21 20:14:20 +0530

    Adding Two num prog

---
+++ javaProject/src/javaProject/AddingTwoNumber.java
@@ -0,0 +1,13 @@
+package javaProject;
+
+public class AddingTwoNumber {
+
+	public static void main(String[] args) {
+		int a =100;
+		int b = 200;
+		int sum = a+b;
+		System.out.println("Adding Two Numbers:"+sum);
+
+	}
+
+}
