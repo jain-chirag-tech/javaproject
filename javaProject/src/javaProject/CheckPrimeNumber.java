f151ac8d23ee49abf2c09440a57702bff524ce81
chirag <chiragjn807@gmail.com>
2020-05-23 11:57:13 +0530

    checking prime number

---
+++ javaProject/src/javaProject/CheckPrimeNumber.java
@@ -0,0 +1,28 @@
+package javaProject;
+
+import java.util.Scanner;
+
+public class CheckPrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number = ");
+		int n = sc.nextInt();
+		boolean isPrime = true;
+		for(int i=2 ; i<=Math.sqrt(n) ; i++) {
+			if (n % i == 0) {
+				isPrime=false;
+				break;	}
+		}
+				
+		if(isPrime) {
+			System.out.println(n + " is PRIME ");
+		}
+		else {
+			System.out.println(n + " is NOT PRIME ");
+		}
+		
+	}
+
+}
