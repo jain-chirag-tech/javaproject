93fdaad07cd024cd624f1865c6e13f4c9eca129d
chirag <chiragjn807@gmail.com>
2020-05-23 15:27:05 +0530

    Amstrong Number

---
+++ javaProject/src/javaProject/AmstrongNumber.java
@@ -0,0 +1,36 @@
+package javaProject;
+
+import java.util.Scanner;
+
+public class AmstrongNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Number = ");
+		
+		int n = sc.nextInt();
+		int temp = 0;
+		temp = n;
+		int sum = 0;
+		while(n>0) {
+			int r = n%10;
+			sum += (r*r*r);
+			n/=10;		
+		}
+	
+		if(temp==sum) {
+			System.out.println(temp +" is Amstrong Number ");
+		}
+		else {
+			System.out.println(temp +" is Not Amstrong Number ");			
+		}		
+		
+
+	}
+	
+		
+	}
+
+
