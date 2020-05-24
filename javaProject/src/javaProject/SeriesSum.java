bb276d135217835cc1334f84f3473db2a87c2865
chirag <chiragjn807@gmail.com>
2020-05-24 16:13:05 +0530

    Series Sum

---
+++ javaProject/src/javaProject/SeriesSum.java
@@ -0,0 +1,21 @@
+package javaProject;
+
+import java.util.Scanner;
+
+public class SeriesSum {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the no = ");
+		int n = sc.nextInt();
+		
+		float sum = 0 ;
+		for(float i=1 ; i<=n ; i++) {
+			sum += 1/i;
+		}
+		System.out.println("Sum of Series = " + sum);
+	}
+
+}
