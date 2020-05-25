0cfcc6b31baa9677cd7efafa11c5848c3607c149
chirag <chiragjn807@gmail.com>
2020-05-25 16:12:22 +0530

    Addition of Matrix

---
+++ javaProject/src/array/MatrixAdd.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.Scanner;
+
+public class MatrixAdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Number Rows = ");
+		int rows = sc.nextInt();
+		
+		System.out.print("Enter Number Column = ");
+		int cols = sc.nextInt();
+		
+		System.out.print("Enter First Matrix = ");
+		int [][] a = new int[rows][cols];
+				
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+			 a[i][j] = sc.nextInt();				
+			}
+		}
+		
+		System.out.print("Enter Second Matrix = ");
+		int [][] b = new int[rows][cols];
+				
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+			 b[i][j] = sc.nextInt();				
+			}
+		}
+		int [][] c = new int[rows][cols];
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+			 c[i][j] = a[i][j] + b[i][j];				
+			}
+		}
+		System.out.println("Sum o f Matrix is ");
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+			System.out.print(c[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
