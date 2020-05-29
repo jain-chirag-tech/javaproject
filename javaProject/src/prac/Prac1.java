1c8d5651be8d241bd32d14b6ce124168f7383c77
chirag <chiragjn807@gmail.com>
2020-05-29 14:17:57 +0530

    practice1

---
+++ javaProject/src/prac/Prac1.java
@@ -0,0 +1,29 @@
+
+//There are three lines of input:
+//
+//The first line contains an integer.
+//The second line contains a double.
+//The third line contains a String.
+
+package prac;
+
+import java.util.Scanner;
+
+public class Prac1 {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+	}
+
+}
