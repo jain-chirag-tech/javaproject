9cab0b872779787a3bb6b7415d16ec3c22eb8524
chirag <chiragjn807@gmail.com>
2020-06-01 13:13:42 +0530

    string practice

---
+++ javaProject/src/prac/Prac7.java
@@ -0,0 +1,29 @@
+package prac;
+
+import java.util.Scanner;
+
+public class Prac7 {
+	
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String A = sc.nextLine();
+		String B = sc.nextLine();
+		
+		System.out.println(A.length()+B.length());
+		int i = A.compareTo(B);
+		if(i>0) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+			
+		String A1 = A.substring(0,1).toUpperCase()+A.substring(1);
+		String B1 = B.substring(0,1).toUpperCase()+B.substring(1);
+		
+		System.out.println(A1 + " " + B);
+		}
+		
+		
+	}
+}
+
