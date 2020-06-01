d143e3b2138c3eb25d284b6518568f2e055c338b
chirag <chiragjn807@gmail.com>
2020-06-01 11:29:23 +0530

    Convert String Into int and vice versa

--- javaProject/src/prac/Prac6.java
+++ javaProject/src/prac/Prac6.java
@@ -1,65 +1,24 @@
-//You are given an integer , you have to convert it into a string.
-//
-//Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
-//
-// can range between -100 to 100 inclusive.
-//
-//Sample Input 0
-//
-//100
-//Sample Output 0
-//
-//Good job
-//
+//How to convert string to int and vice versa .
 
 package prac;
-import java.util.*;
-import java.security.*;
+
+import java.util.Scanner;
+import java.lang.*;
 
 public class Prac6 {
 
 	public static void main(String[] args) {
-		 DoNotTerminate.forbidExit();
-
-		  try {
-		   Scanner in = new Scanner(System.in);
-		   int n = in .nextInt();
-		   in.close();
-		   //String s=???; Complete this line below
-
-		   String s = Integer.toString(n);
-
-		   
-		   if (n == Integer.parseInt(s)) {
-		    System.out.println("Good job");
-		   } else {
-		    System.out.println("Wrong answer.");
-		   }
-		  } catch (DoNotTerminate.ExitTrappedException e) {
-		   System.out.println("Unsuccessful Termination!!");
-		  }
-		 }
-		}
-
-		//The following class will prevent you from terminating the code using exit(0)!
-		class DoNotTerminate {
-
-		 public static class ExitTrappedException extends SecurityException {
-
-		  private static final long serialVersionUID = 1;
-		 }
-
-		 public static void forbidExit() {
-		  final SecurityManager securityManager = new SecurityManager() {
-		   @Override
-		   public void checkPermission(Permission permission) {
-		    if (permission.getName().contains("exitVM")) {
-		     throw new ExitTrappedException();
-		    }
-		   }
-		  };
-		  System.setSecurityManager(securityManager);
+		Scanner sc = new Scanner(System.in);
+		sc.close();
 		
+		int n = 100;
+		String str1 = Integer.toString(n);
+		System.out.println(str1);
+		
+		String str2 = "101";
+		int x = Integer.parseInt(str2);	
+		System.out.println(x);
+
 	}
 
 }
