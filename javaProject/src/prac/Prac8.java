d4ac3eeb781dc63a58219287519d71d3e3bb931b
chirag <chiragjn807@gmail.com>
2020-06-01 17:23:10 +0530

    Practice8

---
+++ javaProject/src/prac/Prac8.java
@@ -0,0 +1,26 @@
+//Java Substring
+
+package prac;
+
+import java.util.Scanner;
+
+public class Prac8 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+//		String s = sc.next();
+//		int start = sc.nextInt();
+//		int end = sc.nextInt();
+//		System.out.println(s.substring(start, end));
+   
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		String s = sc.next();
+		System.out.println(start);
+		System.out.println(end);
+		System.out.println(s);
+	}
+
+}
