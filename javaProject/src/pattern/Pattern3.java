4aa8a22d0c02a8ea1690155adadfe69afbc6f2a6
chirag <chiragjn807@gmail.com>
2020-06-09 22:07:54 +0530

    Pattern 3

---
+++ javaProject/src/pattern/Pattern3.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i =0 ; i<5 ; i++) {
+			for(int j = 5-i ; j>0 ; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
