d34c9f5b9b5ccc0d51d59aa47e9452a105a515eb
chirag <chiragjn807@gmail.com>
2020-06-12 14:34:36 +0530

    Linkedlist Class

---
+++ javaProject/src/dataStructure/Linkedlist.java
@@ -0,0 +1,16 @@
+package dataStructure;
+
+public class Linkedlist {
+	
+	public static void main(String [] args) {
+		MyLinkedList llist = new MyLinkedList();
+		llist.add(45);
+		llist.add(4);
+		llist.add(5);
+		llist.add(87);
+		llist.add(12);
+		llist.add(16);
+	
+		llist.print();		
+	}
+}
