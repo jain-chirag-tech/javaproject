e666c4dc00ce3b69a621571599271b881ba52327
chirag <chiragjn807@gmail.com>
2020-07-26 15:28:41 +0530

    Insertion Sort

---
+++ javaProject/src/sorting/Insertion_Sort.java
@@ -0,0 +1,43 @@
+package sorting;
+
+public class Insertion_Sort {
+
+/*	public static void sort(int []arr) {
+		int n = arr.length;
+		for(int i=1; i<n; i++) {
+			int temp = arr[i];
+			int hole = i;
+			while(arr[hole-1]>temp && hole>0) {
+				arr[hole] = arr[hole-1];
+				hole--;
+			}
+			arr[hole] = temp;
+		}
+	}	
+	*/
+	static void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i=1; i<n; ++i)
+        {
+            int key = arr[i];
+            int j = i-1;
+           
+            while (j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+	
+	public static void main(String [] args) {
+		int arr[] = {5,2,1,9,7,6};
+		sort(arr);
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
