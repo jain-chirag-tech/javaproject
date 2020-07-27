d7d118ca877d0e38d02bcdf943e82a881432067a
chirag <chiragjn807@gmail.com>
2020-07-27 17:24:26 +0530

    First Bad Version

---
+++ javaProject/src/L278.java
@@ -0,0 +1,28 @@
+/*
+ * https://leetcode.com/problems/first-bad-version/
+ */
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+boolean isBadVersion(int version); */
+
+public class L278 {
+//	   public int firstBadVersion(int n) {
+//		      int i = 1;
+//			int j = n;
+//
+//			while (i < j) {
+//
+//				int mid = i + (j - i) / 2;
+//
+//				if (isBadVersion(mid)) {
+//					j = mid;
+//				} else {
+//					i = mid + 1;
+//				}
+//
+//			}
+//
+//			return i;
+//
+//		    }
+}
