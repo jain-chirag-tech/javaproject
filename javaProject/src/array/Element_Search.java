1a1f4985a4def2401bf381a01b1b90d0cba73a6b
chirag <chiragjn807@gmail.com>
2020-09-01 22:08:40 +0530

    Searching Element in Array

---
+++ javaProject/src/array/Element_Search.java
@@ -0,0 +1,29 @@
+package array;
+
+public class Element_Search {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {24,18,12,30,11,7};
+		int x = 30;
+		int position = Search_Element(arr,x);
+		if(position == -1) 
+		{
+			System.out.println("Element Not Found");
+		}
+		else
+		{
+			System.out.println("Element Found at Position = "+ (position+1));
+		}
+		
+	}
+	
+	public static int Search_Element(int arr[],int x){
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]==x)
+				return i;
+		}
+			return -1;		
+	}	
+}
