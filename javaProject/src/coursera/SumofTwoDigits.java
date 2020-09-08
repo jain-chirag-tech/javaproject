0754d7b533cbc45c9e3998a24634ee9f6ddcdef9
chirag <chiragjn807@gmail.com>
2020-09-08 21:36:43 +0530

    Sum of Two Digits

---
+++ javaProject/src/coursera/SumofTwoDigits.java
@@ -0,0 +1,14 @@
+package coursera;
+
+import java.util.Scanner;
+
+public class SumofTwoDigits {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        System.out.println(x+y);
+        s.close();
+    }
+}
