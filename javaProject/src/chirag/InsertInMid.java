0594afbf8ad98e4f3736d94b4e18426933ca8a12
chirag <chiragjn807@gmail.com>
2020-09-08 21:37:36 +0530

    change

--- javaProject/src/chirag/InsertInMid.java
+++ javaProject/src/chirag/InsertInMid.java
@@ -1,3 +1,23 @@
+/*
+ Input:
+LinkedList = 1->2->4
+key = 3
+Output: 1 2 3 4
+Explanation: The new element is inserted
+after the current middle element in the
+linked list.
+Example 2:
+
+Input:
+LinkedList = 10->20->40->50
+key = 30
+Output: 10 20 30 40 50
+Explanation: The new element is inserted
+after the current middle element in the
+linked list and Hence, the output is
+10 20 30 40 50.
+ */
+
 package chirag;
 
 public class InsertInMid {
