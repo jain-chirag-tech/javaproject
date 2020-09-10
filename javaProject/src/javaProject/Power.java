a83d5dd82251631d915a4d3989c8b4ccc46b31ea
chirag <chiragjn807@gmail.com>
2020-09-10 20:43:14 +0530

    change

--- javaProject/src/javaProject/Power.java
+++ javaProject/src/javaProject/Power.java
@@ -12,15 +12,13 @@ public class Power {
 		int a =sc.nextInt();
 		System.out.print("Enter Exponent Value = ");
 		int b =sc.nextInt();
+		sc.close();
 		
 		int result = 1;
 		
 		for(int i=1; i<=b; i++) {
 			result*=a;
 		}
-		System.out.println(a + " raise to power "+ b + " = "+ result);		
-		
-
+		System.out.println(a + " raise to power "+ b + " = "+ result);	
 	}
-
 }
