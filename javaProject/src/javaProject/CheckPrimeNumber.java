670a39d5cfd07de9f90f6e0178ec0ba01a7564b7
chirag <chiragjn807@gmail.com>
2020-09-11 21:22:13 +0530

    change

--- javaProject/src/javaProject/CheckPrimeNumber.java
+++ javaProject/src/javaProject/CheckPrimeNumber.java
@@ -9,6 +9,7 @@ public class CheckPrimeNumber {
 		
 		System.out.println("Enter the number = ");
 		int n = sc.nextInt();
+		sc.close();
 		boolean isPrime = true;
 		for(int i=2 ; i<=Math.sqrt(n) ; i++) {
 			if (n % i == 0) {
