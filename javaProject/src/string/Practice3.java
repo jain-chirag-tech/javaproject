6ad9f0be527565f55bfb7fc509c03bf94c807981
chirag <chiragjn807@gmail.com>
2020-09-12 21:01:50 +0530

    change

--- javaProject/src/string/Practice3.java
+++ javaProject/src/string/Practice3.java
@@ -15,14 +15,14 @@ public class Practice3 {
 	   
 	 //  System.out.println(name);
 	   
-//	   System.out.println(name.charAt(2));				//i
-//	   System.out.println(name.length());				//6
-//	   System.out.println(name3.contains("hira"));		//true
-//	   System.out.println(name3.substring(12));			//good boy
-//	   System.out.println(name3.substring(12,16));		//good
-//	   System.out.println(name.isEmpty()); 				//false
-//	   System.out.println(name.equals(name2));			//false
-//	   System.out.println(name.equals(name4));			//true
+	   System.out.println(name.charAt(2));				//i
+	   System.out.println(name.length());				//6
+	   System.out.println(name3.contains("hira"));		//true
+	   System.out.println(name3.substring(12));			//good boy
+	   System.out.println(name3.substring(12,16));		//good
+	   System.out.println(name.isEmpty()); 				//false
+	   System.out.println(name.equals(name2));			//false
+	   System.out.println(name.equals(name4));			//true
 	   System.out.println(name.equals(name5));			//true
 	   System.out.println(name==name5);					//false
 	   //concept of string pool area and heap memory
