c3459be02f7c9ff6d5fa5ab9ab6792b11511e0fd
chirag <chiragjn807@gmail.com>
2020-09-21 21:22:25 +0530

    Conversion of integer and string

--- javaProject/src/prac/Prac6.java
+++ javaProject/src/prac/Prac6.java
@@ -2,15 +2,10 @@
 
 package prac;
 
-import java.util.Scanner;
-import java.lang.*;
-
 public class Prac6 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.close();
-		
+				
 		int n = 100;
 		String str1 = Integer.toString(n);
 		System.out.println(str1);
