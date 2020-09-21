f1b8dd6187776b8b4ad6c77b51482f2aa331a9cf
chirag <chiragjn807@gmail.com>
2020-09-21 21:21:54 +0530

    SubString

--- javaProject/src/prac/Prac7.java
+++ javaProject/src/prac/Prac7.java
@@ -9,6 +9,7 @@ public class Prac7 {
 		
 		String A = sc.nextLine();
 		String B = sc.nextLine();
+		sc.close();
 		
 		System.out.println(A.length()+B.length());
 		int i = A.compareTo(B);
@@ -20,7 +21,7 @@ public class Prac7 {
 		String A1 = A.substring(0,1).toUpperCase()+A.substring(1);
 		String B1 = B.substring(0,1).toUpperCase()+B.substring(1);
 		
-		System.out.println(A1 + " " + B);
+		System.out.println(A1 + " " + B1);
 		}
 		
 		
